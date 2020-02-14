c4256f4cb019ccef468a272b50d92df509ee28d3
kondo <kondo@ferix.jp>
2020-02-14 13:04:53 +0900

    deploy test commit

---
+++ Handler.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Threading.Tasks;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+
+[assembly : LambdaSerializer (typeof (Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace AwsDotnetCsharp
+{
+  public class Handler
+  {
+    public async Task<APIGatewayProxyResponse> FunctionHandler (APIGatewayProxyRequest gatewayProxyRequest, ILambdaContext context)
+    {
+      Console.WriteLine (gatewayProxyRequest);
+      return new APIGatewayProxyResponse ();
+    }
+  }
+}
