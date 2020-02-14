79953a30a2301000d89645640f5cf4e4672bce75
kondo <kondo@ferix.jp>
2020-02-14 13:56:24 +0900

    ログ出力修正

--- Handler.cs
+++ Handler.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading.Tasks;
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
+using Newtonsoft.Json;
 
 [assembly : LambdaSerializer (typeof (Amazon.Lambda.Serialization.Json.JsonSerializer))]
 
@@ -11,8 +12,12 @@ namespace AwsDotnetCsharp
   {
     public async Task<APIGatewayProxyResponse> Process (APIGatewayProxyRequest gatewayProxyRequest, ILambdaContext context)
     {
-      Console.WriteLine (gatewayProxyRequest);
-      return new APIGatewayProxyResponse ();
+      Console.WriteLine (JsonConvert.SerializeObject (gatewayProxyRequest));
+      return new APIGatewayProxyResponse
+      {
+        StatusCode = 200,
+        Body = "JsonConvert.SerializeObject (body)"
+      };
     }
   }
 }
